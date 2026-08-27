313b118136884e112e9f729f1dd97faf0ffcb95f
ArnoldLakra <arnoldlakra7650@gmail.com>
2026-08-27 21:34:08 +0530

    adding comments

--- 01-variables-datatypes-io/01-variables-datatypes-io/01-hello-world.c
+++ 01-variables-datatypes-io/01-variables-datatypes-io/01-hello-world.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-int main () {
-    printf("hello world");
-    return 0;
+#include<stdio.h>                //header file
+int main () {                   //int main where executions of the program starts
+    printf("hello world");     //this is a output file ehich is used to print a string
+    return 0;                 //return 0 at the end of the program 
 }
