59a0d546cd060057f4032fc7e1cd309ae342b6e1
ArnoldLakra <arnoldlakra7650@gmail.com>
2026-08-30 16:41:26 +0530

    Add positive negetive zero checker

---
+++ 03-conditional-statements/positive-negetive-zero.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main () {
+    int a;
+    printf("enter an integer: ");
+    scanf("%d", &a);
+    if(a>0) {
+        printf("positive");
+    } else if(a==0) {
+        printf("zero");
+    } else {
+        printf("negetive");
+    }
+    return 0;
+}
