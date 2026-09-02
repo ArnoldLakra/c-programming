deb96af2ac5903e0112ec79b04961bc67781f779
ArnoldLakra <arnoldlakra7650@gmail.com>
2026-09-02 21:10:51 +0530

    Add check number range

--- 02-instructions-and-operators/06-number-range.c
+++ 02-instructions-and-operators/06-number-range.c
@@ -3,7 +3,7 @@ int main () {
     // Take a number and check whether it is between 10 and 50, including 10 and 50.
     // 1 is true and 0 is false
     int number;
-    printf("enter a number: ");
+    printf("enter a number within (10-50):  ");
     scanf("%d", &number);
     printf("The number is between 10 and 50: %d", 10<=number && number<=50);
     return 0;
