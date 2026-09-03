9ff2348debcd65ec9f7ad9a76e7f1addd01c8c42
ArnoldLakra <arnoldlakra7650@gmail.com>
2026-09-03 21:31:04 +0530

    Add divisible by both

--- 02-instructions-and-operators/12-divisible-by-both.c
+++ 02-instructions-and-operators/12-divisible-by-both.c
@@ -3,8 +3,8 @@ int main () {
     // if the number is divisible by both 3 and 5
     // 1 means yes and 0 means no
     int n;
-    printf("enter a number: ");
+    printf("enter a number:  ");
     scanf("%d", &n);
-    printf("%d", n%3==0 && n%5==0);
+    printf("%d", n%3==0 && n%5==0);      
     return 0;
 }
