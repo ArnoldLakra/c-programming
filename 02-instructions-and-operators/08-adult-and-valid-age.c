ee68261749166f5efa065c147d3c2bc467b576c3
ArnoldLakra <arnoldlakra7650@gmail.com>
2026-09-03 21:25:20 +0530

    Add adult verifier

--- 02-instructions-and-operators/08-adult-and-valid-age.c
+++ 02-instructions-and-operators/08-adult-and-valid-age.c
@@ -3,7 +3,7 @@ int main () {
     // Is the person at least 18 and less than or equal to 60 ?
     // 1 is true and 0 is false
     int age;
-    printf("enter your age: ");
+    printf("enter your age:  ");
     scanf("%d", &age);
     printf("%d", age>=18 && age<=60);
     return 0;
