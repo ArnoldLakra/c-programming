16f8426c865f3f85b6e026a3c29c684b8ebb07ca
ArnoldLakra <arnoldlakra7650@gmail.com>
2026-09-03 21:21:20 +0530

    Add marks checker

--- 02-instructions-and-operators/07-marks-check.c
+++ 02-instructions-and-operators/07-marks-check.c
@@ -3,7 +3,7 @@ int main () {
     // Take marks and print whether the marks are greater than or equal to 40.
     // 1 is true and 0 is false
     int marks;
-    printf("enter your marks: ");
+    printf("enter your marks:  ");
     scanf("%d", &marks);
     printf("your marks are greater or equal to 40: %d", marks>=40 && marks<=100);
     return 0;
