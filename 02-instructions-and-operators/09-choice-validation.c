ddea246d9544d91f5f2b7186a1e07e1fd9feacfb
ArnoldLakra <arnoldlakra7650@gmail.com>
2026-09-03 21:27:10 +0530

    Add choice validation

--- 02-instructions-and-operators/09-choice-validation.c
+++ 02-instructions-and-operators/09-choice-validation.c
@@ -2,7 +2,7 @@
 int main () {
     // 1 is true and 0 is false
     int a;
-    printf("enter a number: ");
+    printf("enter a number:  ");
     scanf("%d", &a);
     printf("your number is either 1 or 2: %d", a==1 || a==2);
     return 0;
