91a82ef13a1d8e2728dd1ee1f58e1a15d5ae8aec
ArnoldLakra <arnoldlakra7650@gmail.com>
2026-09-03 21:28:52 +0530

    Add not zero

--- 02-instructions-and-operators/10-not-zeo.c
+++ 02-instructions-and-operators/10-not-zeo.c
@@ -3,7 +3,7 @@ int main () {
     // is the number not equal to zero
     // 1 is true and 0 is false
     int a;
-    printf("enter a number: ");
+    printf("enter a number:  ");
     scanf("%d", &a);
     printf("the number is non-zero: %d", a!=0);
     return 0;
