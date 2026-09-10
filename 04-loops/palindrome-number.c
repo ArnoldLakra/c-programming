58cb86b27f2ec8666df23ce2c9f1ad53956ffbc1
ArnoldLakra <arnoldlakra7650@gmail.com>
2026-09-10 17:50:07 +0530

    Add palindrome number checker

--- 04-loops/palindrome-number.c
+++ 04-loops/palindrome-number.c
@@ -7,7 +7,7 @@ int main() {
     while(n>0) {
         digit=n%10;          // this digit will take the last digit of the number and in the next loop it will take the next number
         n=n/10;              // this will remove the last digit from the n number
-        reverse=reverse*10+digit;   // here's the logic reverse=0*10+1 then in the next loop reverse=1*10+2 which will give a new number
+        reverse=reverse*10+digit;   // putting that digit, here's the logic reverse=0*10+1 then in the next loop reverse=1*10+2 which will give a new number
     }  // i have not put an iteration becasue the n is already getting lower and will gradually become 0
     printf("%d %d\n", original, reverse);
     if(original==reverse) {
