1c57fff8d160a2119b99397807cedb85121f187f
ArnoldLakra <arnoldlakra7650@gmail.com>
2026-09-11 20:59:13 +0530

    Add prime numbers in range

--- 04-loops/prime-numbers-in-range.c
+++ 04-loops/prime-numbers-in-range.c
@@ -3,15 +3,12 @@ int main() {
     int n;
     printf("enter a number: ");
     scanf("%d", &n);
-    for(int i=1; i<=n; i++) {   // runs the code till the number n
+    for(int i=2; i<=n; i++) {   // runs the code till the number n
         int isprime=1;          // isprime is written in for loop becuase the process has to repeat again for a new number check
-        if(i<2) {
-        } else {
-            for(int j=2; j<=i-1; j++) {    // checking if the number is prime
-                if(i%j==0) {
-                    isprime=0;
-                    break;
-                }
+        for(int j=2; j<=i-1; j++) {
+            if(i%j==0) {
+                isprime=0;
+                break;
             }
         }
         if(isprime==1) {
